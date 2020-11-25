71c4417e879d48b004aa461e1fd2ea71e58bf87f
Tommy Phoenix Gatti <u6044453@anu.edu.au>
2020-11-25 23:17:04 +1100

    Filling out Node Component updated Canvas.js: basic transitionary example updated Node.{js,css}: added node types, transitions, editable text within

--- src/Canvas.js
+++ src/Canvas.js
@@ -17,32 +17,40 @@ const nodeVariants = {
   }
 };
 
+const literalRegex = /".*"(\^\^.*|@.*)?|true|false|[+-]?\d+|[+-]?\d*\.\d+|[+-]?(\d+\.\d*[eE][+-]?\d+|\.\d+[eE][+-]?\d+|\d+[eE][+-]?\d+)/;
+
 export default class Canvas extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.state ={content: '?'}
   }
 
   handleClick(event){
     this.props.onSelectedItemChange(event.target.textContent);
   }
 
+  handleTextChange(event){
+    this.setState({content: event.target.value});
+  }
+
   render() {
-    const unknown = 'unknown';
     const animation = 'knownLit';
+    const content = this.state.content;
 
     return (
       <div className="canvas">
         <motion.div
           className="known" drag dragMomentum={false} onTap={this.handleClick}
           variants={nodeVariants}
-          initial={unknown}
+          initial={'unknown'}
           animate={animation}
           transition={{duration: 4}}
         >
-          <p style={{position: "center"}}>?</p>
+          <input style={{textAlign: 'center', background: "transparent", border: "none"}} value={content} onChange={this.handleTextChange}/>
         </motion.div>
       </div>
     );
   }
-}
+} //textalign doesn't work!
--- src/Node.js
+++ src/Node.js
@@ -2,24 +2,83 @@ import React from "react";
 import { motion } from "framer-motion";
 import "./Node.css";
 
+const nodeType = {
+  unknown:{
+    backgroundColor: '#0000fe',
+    borderRadius: '50%'
+  },
+  unknownopt:{
+    backgroundColor: '#1e90ff',
+    borderRadius: '50%',
+    border: 'dashed 3px #0000fe'
+  },
+  selectedunknown:{
+    backgroundColor: '#0000fe',
+    borderRadius: '50%'
+  },
+  selectedunknownopt:{
+    backgroundColor: '#1e90ff',
+    borderRadius: '50%',
+    border: 'dashed 3px #0000fe'
+  },
+  uri:{
+    backgroundColor: '#4e4e4e',
+    borderRadius: '50%'
+  },
+  uriopt:{
+    backgroundColor: '#bebebe',
+    borderRadius: '50%',
+    border: '3px dashed #4e4e4e'
+  },
+  literal:{
+    backgroundColor: '#4e4e4e',
+    borderRadius: '50%'
+  },
+  literalopt:{
+    backgroundColor: '#bebebe',
+    borderRadius: '50%',
+    border: '3px dashed #4e4e4e'
+  },
+  amalgam:{
+    backgroundColor: '#444444',
+    borderRadius: '10%'
+  },
+};
+
 export default class Node extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChangedText = this.handleChangedText.bind(this);
     this.state = {
-      type: 'untyped',
+      type: nodeType.unknown,
+      isOptional: false,
       prefix: '',
-      label: ''
+      content: '?'
     }
   }
 
-  render(){
-    const type = this.state.type;
-    const prefix = this.state.prefix;
-    const label = this.state.label;
+  handleClick(e){
+    this.props.onSelectedItemChange(e)
+  }
 
+  handleChangedText(e){
+    this.setState({content: e.target.textContent});
+  }
+
+  render(){
+    const { type, isOptional, content } = this.state;
+    const animation = type + isOptional ? "opt" : "";
+
+    //todo: could use onfocusout event rather than onchange
     return (
-      <motion.div className={"node"}>
-          <p>{prefix + ':' + label}</p>
+      <motion.div className={"node"}
+                  drag dragMomentum={false} onTap={this.handleClick}
+                  variants={nodeType} initial={'unknown'} animate={animation} transition={{duration: 2}}
+      >
+          <input className={"transparentInput"}
+                 value={content}
+                 onChange={this.handleChangedText}/>
       </motion.div>
     );
   }
