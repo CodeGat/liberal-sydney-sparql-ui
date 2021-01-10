ba3c5a8f50b0f50306d29754aae40e7ba6d3792e
Tommy Phoenix Gatti <tommy.gatti@outlook.com>
2021-01-10 19:35:42 +1100

    Updated Node.js: cut down on Node file by adding isOptional parameter to variant rather than variants in their own right

--- src/Node.js
+++ src/Node.js
@@ -3,61 +3,44 @@ import { motion } from "framer-motion";
 import "./Node.css";
 import "./Canvas.css";
 
-//todo: something wrong with animating to borderRadius
-const NODE_TYPE = {
-  unknown:{
-    fill: '#0000fe',
+const NODE_VARIANTS = {
+  unknown: isOpt => ({
+    fill: isOpt ? '#1e90ff' : '#0000fe',
     rx: 50,
     height: "100px",
-    width: '100px'
-  },
-  unknownopt:{
-    fill: '#1e90ff',
-    rx: 50,
-    border: '3px dashed #0000fe',
-    height: "100px",
-    width: '100px'
-  },
-  selectedunknown:{
-    fill: '#0000fe',
+    width: '100px',
+    strokeWidth: isOpt ? 5 :  0,
+    strokeDasharray: 3,
+    stroke: '#0000fe'
+  }),
+  selectedunknown: isOpt => ({
+    fill: isOpt ? '1e90ff' : '#0000fe',
     rx: 50,
     height: "100px",
-    width: '100px'
-  },
-  selectedunknownopt:{
-    fill: '#1e90ff',
-    rx: 50,
-    border: '3px dashed #0000fe',
-    height: "100px",
-    width: '100px'
-  },
-  uri:{
-    fill: '#4e4e4e',
+    width: '100px',
+    strokeWidth: isOpt ? 5 :  0,
+    strokeDasharray: 3,
+    stroke: '#0000fe'
+  }),
+  uri: isOpt => ({
+    fill: isOpt ? '#4e4e4e' : '#bebebe',
     rx: 50,
     height: "100px",
-    width: '100px'
-  },
-  uriopt:{
-    fill: '#bebebe',
-    rx: 50,
-    border: '3px dashed #4e4e4e',
-    height: "100px",
-    width: '100px'
-  },
-  literal:{
-    fill: '#4e4e4e',
+    width: '100px',
+    strokeWidth: isOpt ? 5 :  0,
+    strokeDasharray: 3,
+    stroke: '#0000fe'
+  }),
+  literal: isOpt => ({
+    fill: isOpt ? '#bebebe' : '#4e4e4e',
     rx: 0,
     height: "100px",
-    width: "200px"
-  },
-  literalopt:{
-    fill: '#bebebe',
-    rx: 0,
-    border: '3px dashed #4e4e4e',
-    height: "100px",
-    width: '200px'
-  },
-  amalgam:{
+    width: "200px",
+    strokeWidth: isOpt ? 5 :  0,
+    strokeDasharray: 3,
+    stroke: '#0000fe'
+  }),
+  amalgam: {
     fill: '#444444',
     height: "100px",
     width: "100px",
@@ -66,7 +49,7 @@ const NODE_TYPE = {
   unf: {
     width: '40px',
     height: '40px'
-  }
+  },
 };
 
 const NODE_HEIGHT = 100, NODE_WIDTH = 100, LITERAL_WIDTH = 200;
@@ -115,7 +98,6 @@ export default class Node extends React.Component {
 
   render(){
     const { type, isOptional, content } = this.state;
-    const rectAnimation = type + (isOptional ? "opt" : "");
     const { mode, init, x, y } = this.props;
     const adjustedX = x - NODE_WIDTH / 2;
     const adjustedY = y - NODE_HEIGHT / 2;
@@ -124,8 +106,8 @@ export default class Node extends React.Component {
     return (
       <motion.g drag dragMomentum={false} whileHover={{scale: 1.2}}>
         <motion.rect x={adjustedX} y={adjustedY} onClickCapture={this.handleEntryExit}
-                     variants={NODE_TYPE} initial="unknown" animate={rectAnimation} transition={{duration: 0.5}}
-                     transformTemplate={() => "translateX(0) translateY(0)"}/>
+                     variants={NODE_VARIANTS} initial="unknown" animate={type} custom={isOptional}
+                     transition={{duration: 0.5}} transformTemplate={() => "translateX(0) translateY(0)"}/>
         <foreignObject x={adjustedX - (LABEL_WIDTH - currentNodeWidth) / 2} y={adjustedY}
                               width={LABEL_WIDTH} height={LABEL_HEIGHT}>
           <motion.input className={"nodeLabel"} value={content} disabled={mode === "edge"}
