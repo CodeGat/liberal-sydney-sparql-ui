595b77370bf4c61550b58a53b02c08d478407872
Tommy Phoenix Gatti <tommy.gatti@outlook.com>
2021-02-06 00:28:38 +1100

    Fixed warning in Node.js

--- src/Node.js
+++ src/Node.js
@@ -118,7 +118,8 @@ export default class Node extends React.Component {
 
   render(){
     const { type, isOptional, content, adjustedX, adjustedY } = this.state;
-    const { mode, init } = this.props;
+    // const { mode, init } = this.props;
+    const { mode } = this.props;
     const currentNodeWidth = type.match(/uri|unknown/) ? Node.nodeWidth : Node.literalWidth;
 
     return (
