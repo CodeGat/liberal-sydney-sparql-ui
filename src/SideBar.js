58a9d41bcab9fed39c79410507f5b380140e2f6f
Tommy Phoenix Gatti <tommy.gatti@outlook.com>
2021-02-10 15:02:04 +1100

    Added a basic component-based SelectItemViewer and propagated changes to the data downwards as noted in Issue #17, as well as added images for other types of selected icons.

--- src/SideBar.js
+++ src/SideBar.js
@@ -5,6 +5,8 @@ import "./Sidebar.css";
 import arrowImg from './arrow_icon_black.png';
 import nodeImg from './node_icon_known.png';
 import litImg from './literal_icon_known.png';
+import noneImg from './none_icon.png';
+import unkImg from './node_icon_unknown.png'
 
 export default class SideBar extends React.Component {
   constructor(props) {
@@ -42,21 +44,12 @@ export default class SideBar extends React.Component {
   render(){
     const { content, type, id } = this.props.selected;
     const { info, infoLoaded } = this.state;
-    const selectedComponents = content.split(':');
-    let prefix, name;
-
-    if (selectedComponents.length === 1){ //todo: see data rework issue
-      prefix = ''
-      name = content;
-    } else {
-      [ prefix, name ] = selectedComponents;
-    }
 
     return (
       <div className="sidebar">
-        <SelectedItemViewer type={type} prefix={prefix} name={name}
+        <SelectedItemViewer type={type} content={content}
                             info={info} infoLoaded={infoLoaded} />
-        <SuggestiveSearch id={id} type={type} prefix={prefix} name={name}
+        <SuggestiveSearch id={id} type={type} content={content}
                           info={info} infoLoaded={infoLoaded} />
       </div>
     );
@@ -71,35 +64,160 @@ class SelectedItemViewer extends React.Component {
   }
 
   render() {
-    const { type, prefix, name } = this.props;
+    const { type, content } = this.props;
+
+    if (type === "uri") {
+      // const [prefix, name] = content.split(/[.#/](?=[^.#/]*$)/); //todo: used for full uris!
+      const [prefix, name] = content.split(':');
+
+      return (<SelectedUriItemViewer type={type} prefix={prefix} name={name}/>);
+    } else if (type === "unknown") {
+      return (<SelectedUnknownItemViewer type={type} content={content} />);
+    } else if (type === "literal") {
+      return (<SelectedLiteralItemViewer type={type} content={content} />);
+    } else if (type === "edge") {
+      return (<SelectedEdgeItemViewer type={type} content={content} />); //todo: doesn't include distinction between ? and uri edges! look in edge
+    } else {
+      return (<SelectedNullItemViewer type={type} content={content} />);
+    }
+  }
+}
+
+function SelectedUriItemViewer(props) {
+  const { type, prefix, name } = props;
+
+  return (
+    <div className={'itemviewer'}>
+      <SelectedItemViewerImageHeader type={type} name={name} />
+      <SelectedItemViewerPrefix prefix={prefix} />
+    </div>
+  );
+}
+
+function SelectedUnknownItemViewer(props) {
+  const { type, content } = props;
+  return (
+    <div className={'itemviewer'}>
+      <SelectedItemViewerImageHeader type={type} name={content} />
+      <SelectedItemViewerInferredProps />
+    </div>
+  );
+}
+
+function SelectedLiteralItemViewer(props) {
+  const { type, content } = props;
+
+  const name = content.match(/".*".*/) ? content.split(/(?=[^"]*$)/)[0] : content;
+
+  return (
+    <div className={'itemviewer'}>
+      <SelectedItemViewerImageHeader type={type} name={name} />
+      <SelectedItemViewerLiteralType content={content} />
+    </div>
+  );
+}
+
+//todo: remember about the case of ? edges!
+function SelectedEdgeItemViewer(props) {
+  const { type, content } = props;
+  const [name, prefix] = content.split(":");
+
+  return (
+    <div className={'itemviewer'}>
+      <SelectedItemViewerImageHeader type={type} name={name} />
+      <SelectedItemViewerPrefix prefix={prefix} />
+    </div>
+  );
+}
+
+function SelectedNullItemViewer(props) {
+  const { type } = props;
+
+  return (
+    <div className={'itemviewer'}>
+      <SelectedItemViewerImageHeader type={type} />
+    </div>
+  );
+}
+
+function SelectedItemViewerImageHeader(props) {
+  const { type, name } = props;
+
+  if (type === 'uri') {
+    return (
+      <>
+        <img className={'grid-img'} src={nodeImg} alt={'selected known node icon'}/>
+        <p className={'grid-name'}>{name}</p>
+      </>
+    );
+  } else if (type === 'edge') {
+    return (
+      <>
+        <img className={'grid-img'} src={arrowImg} alt={'selected known edge icon'}/>
+        <p className={'grid-name'}>{name}</p>
+      </>
+    );
+  } else if (type === 'literal') {
+    return (
+      <>
+        <img className={'grid-img'} src={litImg} alt={'selected known literal icon'}/>
+        <p className={'grid-name'}>{name}</p>
+      </>
+    );
+  } else if (type === 'unknown') {
+    return (
+      <>
+        <img className={'grid-img'} src={unkImg} alt={'selected unknown node icon'} />
+        <p className={'grid-name'}>{name}</p>
+      </>
+    );
+  } else {
+    console.log("Unknown type: " + type);
 
     return (
-      <div id={'itemviewer'}>
-        {type !== '' &&
-          <>
-            <SelectedItemViewerImage type={type} />
-            <p className={'grid-name'}>{name}</p>
-            <p className={'grid-from'}>From</p>
-            <p className={'grid-prefix light small'}>{prefix}</p>
-          </>}
-      </div>
+      <>
+        <img className={"grid-img"} src={noneImg} alt={'unknown type icon'} />
+        <p className={'grid-name'}>{type === '' ? 'Nothing is currently selected' : 'Unknown selected item'}</p>
+      </>
     );
   }
 }
 
-function SelectedItemViewerImage(props) {
-  const { type } = props;
+function SelectedItemViewerPrefix(props) {
+  const { prefix } = props;
 
-  if (type === 'uri') {
-    return (<img className={'grid-img'} src={nodeImg} alt={'selected known node icon'}/>);
-  } else if (type === 'edge') {
-    return (<img className={'grid-img'} src={arrowImg} alt={'selected known edge icon'}/>);
-  } else if (type === 'literal') {
-    return (<img className={'grid-img'} src={litImg} alt={'selected known literal icon'}/>);
-  } else {
-    console.warn("Unknown selected item - not explicitly a node, edge or literal");
-    return null;
+  return (
+    <>
+      <p className={'grid-from'}>From</p>
+      <p className={'grid-prefix light small'}>{prefix}</p>
+    </>
+  );
+}
+
+function SelectedItemViewerInferredProps(props) {
+  return null;
+}
+
+function SelectedItemViewerLiteralType(props) {
+  const { content } = props;
+  let type;
+
+  if (content.match(/".*".*/)) {
+    type = "text";
+  } else if (content.match(/true|false/)) {
+    type = "boolean";
+  } else if (content.match(/[+-]?\d+/)) {
+    type = "integer";
+  } else if (content.match(/[+-]?\d*\.\d+|[+-]?(\d+\.\d*[eE][+-]?\d+|\d+[eE][+-]?\d+)/)) {
+    type = "floating point";
   }
+
+  return (
+    <>
+      <p>Type</p>
+      <p>{type}</p>
+    </>
+  );
 }
 
 class SuggestiveSearch extends React.Component {
@@ -112,16 +230,17 @@ class SuggestiveSearch extends React.Component {
     };
   }
 
+  // todo: should node type be in there as well? aka, literal string, literal node, ... oh wait, it is!
   componentDidUpdate(prevProps, prevState, snapshot) {
-    const { id, type, prefix, name } = this.props;
+    const { id, type, content } = this.props;
 
-    if (name !== prevProps.name || prefix !== prevProps.prefix || id !== prevProps.id || type !== prevProps.type){
+    if (content !== prevProps.content || id !== prevProps.id || type !== prevProps.type){
       // generate new suggestions based on the current content
       let newSuggestions;
 
-      if (type === "edge") newSuggestions = this.generateSuggestionsForSelectedEdge(prefix, name);
-      else if (type === "datatype") newSuggestions = this.generateSuggestionsForSelectedDatatype(prefix, name);
-      else newSuggestions = this.generateSuggestionsForSelectedNode(type, prefix, name);
+      if (type === "edge") newSuggestions = this.generateSuggestionsForSelectedEdge(content);
+      else if (type === "datatype") newSuggestions = this.generateSuggestionsForSelectedDatatype(content);
+      else newSuggestions = this.generateSuggestionsForSelectedNode(type, content);
 
       this.setState({suggestions: newSuggestions});
     }
@@ -133,13 +252,13 @@ class SuggestiveSearch extends React.Component {
    * @property {Object} range
    * @property {string} range.prefix
    * @property {string} range.name
-   * @param {string} prefix - prefix of the edge
-   * @param {string} name - name of the edge
+   * @param {string} content - the content of the selected Edge
    * @returns {EdgeSuggestion[]}
    */
-  generateSuggestionsForSelectedEdge(prefix, name) {
+  generateSuggestionsForSelectedEdge(content) {
     const suggestions = [];
     const { elementDefs } = this.state;
+    const [ , name ] = content.split(':');
 
     for (let def of elementDefs) {
       if (def.elem.name === name) {
@@ -158,11 +277,10 @@ class SuggestiveSearch extends React.Component {
    * @typedef {Object} DatatypeSuggestion
    * @property {string} prefix
    * @property {string} name
-   * @param prefix - prefix of the datatype
-   * @param name - name of the datatype
+   * @param content - content of the datatype
    * @returns {DatatypeSuggestion[]}
    */
-  generateSuggestionsForSelectedDatatype(prefix, name) {
+  generateSuggestionsForSelectedDatatype(content) {
     return [];
   }
 
@@ -172,13 +290,13 @@ class SuggestiveSearch extends React.Component {
    * @property {string} prefix - prefix of suggested edge
    * @property {string} name - name of suggested edge
    * @param type - type of the node: is it a literal, iri, unknown?
-   * @param prefix - prefix of the node
-   * @param name - name of the node
+   * @param content - content of the selected node
    * @returns {NodeSuggestion[]}
    */
-  generateSuggestionsForSelectedNode(type, prefix, name) {
+  generateSuggestionsForSelectedNode(type, content) {
     const suggestions = [];
     const { elementDefs } = this.state;
+    const [ , name ] = content.split(':');
 
     for (let def of elementDefs) {
       if (def.domain.name === name) {
@@ -228,7 +346,7 @@ class SuggestiveSearch extends React.Component {
             {defsLoaded && infoLoaded && suggestions.map((s, ix) =>
               <SuggestionWrapper key={ix} suggestion={s} info={info[s.elem.iri]} />)}
             {(!defsLoaded || !infoLoaded) &&
-              <p>Loading...</p>}
+            <p>Loading...</p>}
           </motion.ul>
         </AnimateSharedLayout>
       </div>
@@ -278,10 +396,10 @@ function SuggestionAsNode(props) {
       <p className={"grid-from small"}>From</p>
       <p className={'grid-prefix light small'}>{prefix}</p>
       {comment !== undefined &&
-        <>
-          <p className={'grid-desc small'}>Desc.</p>
-          <p className={'grid-description light small'}>{comment}</p>
-        </>
+      <>
+        <p className={'grid-desc small'}>Desc.</p>
+        <p className={'grid-description light small'}>{comment}</p>
+      </>
       }
     </div>
   );
@@ -325,10 +443,10 @@ function SuggestionForSelectedNode(props) {
       <p className={"grid-from small"}>From</p>
       <p className={'grid-prefix light small'}>{prefix}</p>
       {comment !== undefined &&
-        <>
-          <p className={'grid-desc small'}>Desc.</p>
-          <p className={'grid-description light small'}>{comment}</p>
-        </>
+      <>
+        <p className={'grid-desc small'}>Desc.</p>
+        <p className={'grid-description light small'}>{comment}</p>
+      </>
       }
     </div>
   );
