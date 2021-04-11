02f64534277ff45a366f8bad0db062154b3778c1
Tommy Phoenix Gatti <tommy.gatti@outlook.com>
2021-04-11 17:20:21 +1000

    Node is now stateless - state is held in Canvas entirely

--- src/canvas/Node.js
+++ src/canvas/Node.js
@@ -46,15 +46,15 @@ export default class Node extends React.Component {
       strokeDasharray: 3,
       stroke: '#0000fe'
     }),
-    nodeAmalgam: {
-      fill: '#444444',
-      height: "100px",
-      width: "100px",
+    nodeAmalgam: isOpt => ({
+      fill: isOpt ? '#bebebe' : '#444444',
+      height: 100,
+      width: 100,
       rx: 10,
       ry: 10
-    },
-    nodeUnf: {
-      fill: '#0000fe',
+    }),
+    nodeUnf: isOpt => ({
+      fill: isOpt ? '#1e90ff' : '#0000fe',
       strokeWidth: 0,
       strokeDasharray: 3,
       stroke: '#0000fe',
@@ -62,7 +62,7 @@ export default class Node extends React.Component {
       height: 40,
       rx: 70,
       ry: 70
-    },
+    })
   };
   static nodeHeight = 100;
   static nodeWidth = 100;
@@ -75,40 +75,25 @@ export default class Node extends React.Component {
   //todo: make defaultContent care about prefix! right now it just ignores it
   constructor(props) {
     super(props);
-    this.state = {
-      type: props.init,
-      isOptional: false,
-      prefix: '',
-      content: props.defaultContent,
-      adjustedX: props.x - (props.init === "nodeUnf" ? Node.unfWidth : Node.nodeWidth) / 2,
-      adjustedY: props.y - (props.init === "nodeUnf" ? Node.unfHeight : Node.nodeHeight) / 2
-    };
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    const { init, defaultContent } = this.props;
-
-    if (prevProps.init !== init && prevProps.init === 'nodeUnf'){
-      this.setState({type: init, content: defaultContent});
-    }
-  }
-
+  /**
+   *
+   * @param e - event that triggered the function
+   */
   handleEntryExit = (e) => {
-    const { mode, edgeCompleting, id } = this.props;
-    const { content, type } = this.state;
+    const { mode, edgeCompleting, id, type, isOptional, content, x, y } = this.props;
     e.preventDefault();
 
     if (mode === "edge") {
-      const { x, y } = this.props;
-      const { isOptional, adjustedX, adjustedY } = this.state;
-
       if (edgeCompleting){ // we finish the edge here
         const variant = Node.variants[type](isOptional);
         const info = {id: id, content: content};
         const shape = {
-          x: adjustedX, y: adjustedY,
-          width: parseInt(variant.width),
-          height: parseInt(variant.height),
+          x: x + variant.height / 2,
+          y: y + variant.width / 2,
+          width: variant.width,
+          height: variant.height,
           rx: variant.rx, ry: variant.rx
         };
         const aux = {midX: x, midY: y};
@@ -125,32 +110,42 @@ export default class Node extends React.Component {
     }
   }
 
+  /**
+   * Updates the state of the node with relation to it's text and type
+   * @param e - event that triggered the function
+   */
   handleChangedText = (e) => {
+    const { id } = this.props;
     const changedText = e.target.value;
-    this.setState({content: changedText});
+    let changedType;
 
     if (changedText.match(/".*".*|true|false|[+-]?\d+|[+-]?\d*\.\d+|[+-]?(\d+\.\d*[eE][+-]?\d+|\d+[eE][+-]?\d+)/)){
-      this.setState({type: 'nodeLiteral'});
+      changedType = 'nodeLiteral';
     } else if (changedText.match(/\?.*/)) {
-      this.setState({type: 'nodeUnknown'});
+      changedType = 'nodeUnknown';
     } else {
-      this.setState({type: 'nodeUri'});
+      changedType = 'nodeUri';
     }
+
+    this.props.onChangeNodeState(id, {content: changedText, type: changedType});
   }
 
   render(){
-    const { type, isOptional, content, adjustedX, adjustedY } = this.state;
-    const { mode, init } = this.props;
+    const { mode, type, isOptional, content, x, y } = this.props;
 
-    const currentNodeWidth = type.match(/node(Uri|Unknown)/) ? Node.nodeWidth : Node.literalWidth;
+    const variant = Node.variants[type](isOptional);
+    const currentNodeWidth = variant.width;
+    const currentNodeHeight = variant.height;
+    const actualX = x - currentNodeWidth / 2;
+    const actualY = y - currentNodeHeight / 2;
 
     return (
       <motion.g drag dragMomentum={false} whileHover={{scale: 1.2}}>
-        <motion.rect x={adjustedX} y={adjustedY} onClickCapture={this.handleEntryExit} onMouseUpCapture={() => console.log("mouseup")}
-                     variants={Node.variants} initial={init} animate={type} custom={isOptional}
+        <motion.rect x={actualX} y={actualY} onClickCapture={this.handleEntryExit} onMouseUpCapture={() => console.log("mouseup")}
+                     variants={Node.variants} initial={false} animate={type} custom={isOptional}
                      transition={{duration: 0.5}} transformTemplate={() => "translateX(0) translateY(0)"}/>
         {type !== 'nodeUnf' &&
-          <foreignObject x={adjustedX - (Node.labelWidth - currentNodeWidth) / 2} y={adjustedY + Node.labelHeight}
+          <foreignObject x={actualX - (Node.labelWidth - currentNodeWidth) / 2} y={actualY + Node.labelHeight}
                          width={Node.labelWidth} height={Node.labelHeight}
                          pointerEvents={mode === "edge" ? "none" : "auto"} >
             <motion.input className={"nodeLabel"} value={content} disabled={mode === "edge"}
