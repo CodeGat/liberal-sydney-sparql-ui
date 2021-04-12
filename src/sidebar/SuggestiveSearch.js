3e444f349363b6eb3336ad52f7a6a5c87cd816a4
Tommy Phoenix Gatti <tommy.gatti@outlook.com>
2021-04-12 15:50:51 +1000

    Split SuggestiveSearch and it's SuggestionWrapper counterpart into separate files.

--- src/sidebar/SuggestiveSearch.js
+++ src/sidebar/SuggestiveSearch.js
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
+import React from "react";
+import SuggestionWrapper from "./Suggestion";
 import { submitQuery, fetchPrefixOfExpansion } from "./UtilityFunctions";
-import {ItemDesc, ItemImageHeader, ItemPrefix} from "./ItemViewerComponents";
-import {AnimatePresence, AnimateSharedLayout, motion} from "framer-motion";
+import { AnimateSharedLayout, motion} from "framer-motion";
 import './Sidebar.css';
 import './SuggestiveSearch.css';
 
@@ -11,13 +11,16 @@ export default class SuggestiveSearch extends React.Component {
     this.state = {
       suggestions: [],
       elementDefs: [],
+      baseClasses: [],
       defsLoaded: false,
+      baseClassUsed: false,
       suggestionNo: 0
     };
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    const { id, type, content } = this.props;
+    const { id, type, content, basePrefixLoaded } = this.props;
+    // const { defsLoaded } = this.state;
 
     if (content !== prevProps.content || id !== prevProps.id || type !== prevProps.type){
       // generate new suggestions based on the current content
@@ -30,6 +33,15 @@ export default class SuggestiveSearch extends React.Component {
 
       this.setState({suggestions: newSuggestions});
     }
+    if (!prevProps.basePrefixLoaded && basePrefixLoaded){
+      this.updateStateWithOntologyData();
+    }
+    // if (defsLoaded){
+    //   const { baseClasses } = this.state;
+    //   const baseClassSuggestions = this.generateSuggestionsOfBaseClasses(baseClasses);
+    //
+    //   this.setState({suggestions: baseClassSuggestions});
+    // }
   }
 
   /**
@@ -97,7 +109,7 @@ export default class SuggestiveSearch extends React.Component {
     let ix = suggestionNo;
 
     for (let def of elementDefs) {
-      if (def.domain.label === name) {
+      if (def.domain && def.domain.label === name) {
         suggestions.push({type: 'edgeKnown', elem: def.elem, ix: ix});
         ix++;
       }
@@ -107,6 +119,24 @@ export default class SuggestiveSearch extends React.Component {
 
     return suggestions;
   }
+
+
+  // generateSuggestionsOfBaseClasses = (baseClasses) => {
+  //   const suggestions = [];
+  //   const { suggestionNo } = this.state;
+  //   let ix = suggestionNo;
+  //
+  //   for (const baseClass of baseClasses) {
+  //     suggestions.push({
+  //       type: 'nodeUri',
+  //       elem: baseClass.range,
+  //       ix: ix
+  //     });
+  //     ix++;
+  //   }
+  //
+  //   return suggestions;
+  // }
 
   /**
    * Deletes a suggestion located at ix from the state array of suggestions.
@@ -118,37 +148,47 @@ export default class SuggestiveSearch extends React.Component {
     }));
   }
 
-  componentDidMount() {
+  updateStateWithOntologyData = () => {
     // when component mounts, fetch ontology and the associated data, caching it
     const base_url = "http://localhost:9999/blazegraph/sparql"; //todo: remove local uri
-    submitQuery(base_url, "SELECT DISTINCT ?s ?domain ?range WHERE {" +
+    submitQuery(base_url, "SELECT DISTINCT ?s ?klass ?domain ?range WHERE {" +
+      "OPTIONAL {?s rdf:type ?klass . FILTER (?klass = owl:ObjectProperty || ?klass = owl:Class) }" +
       "OPTIONAL {?s rdfs:domain [ owl:onClass ?domain ] . FILTER (?s != owl:topObjectProperty) } " +
       "OPTIONAL {?s rdfs:range  [ owl:onClass|owl:onDataRange|owl:someValuesFrom ?range ] } }")
       .then(
         response => {
           const results = response.results.bindings;
           const defs = [];
+          const baseClasses = [];
           const myPrefixes = {};
 
-          for (const { s, domain, range } of results) {
+          for (const { s, klass, domain, range } of results) {
+            const def = {};
+
             const [ sExpansion, sName ] = s.value.split("#");
-            const [ dExpansion, dName ] = domain.value.split("#");
-            const [ rExpansion, rName ] = range.value.split("#");
             const sLabel = sName.replace(/_/g, ' ');
-            const dLabel = dName.replace(/_/g, ' ');
-            const rLabel = rName.replace(/_/g, ' ');
-
             const sPrefix = this.findPrefixOfExpansion(sExpansion, myPrefixes);
-            const dPrefix = this.findPrefixOfExpansion(dExpansion, myPrefixes);
-            const rPrefix = this.findPrefixOfExpansion(rExpansion, myPrefixes);
+            def.elem = {iri: s.value, expansion: sExpansion, prefix: sPrefix, name: sName, label: sLabel};
 
-            defs.push({
-              elem: {iri: s.value, expansion: sExpansion, prefix: sPrefix, name: sName, label: sLabel},
-              domain: {iri: domain.value, expansion: dExpansion, prefix: dPrefix, name: dName, label: dLabel},
-              range: {iri: range.value, expansion: rExpansion, prefix: rPrefix, name: rName, label: rLabel}
-            });
+            if (domain) {
+              const [ dExpansion, dName ] = domain.value.split("#");
+              const dLabel = dName.replace(/_/g, ' ');
+              const dPrefix = this.findPrefixOfExpansion(dExpansion, myPrefixes);
+              def.domain = {iri: domain.value, expansion: dExpansion, prefix: dPrefix, name: dName, label: dLabel};
+            }
+            if (range) {
+              const [ rExpansion, rName ] = range ? range.value.split("#") : [null, null];
+              const rLabel = rName.replace(/_/g, ' ');
+              const rPrefix = this.findPrefixOfExpansion(rExpansion, myPrefixes);
+              def.range = {iri: range.value, expansion: rExpansion, prefix: rPrefix, name: rName, label: rLabel};
+            }
+            if (klass && klass.value.split('#')[1] === 'Class') {
+              baseClasses.push({...def.elem});
+            }
+
+            defs.push(def);
           }
-          this.setState({defsLoaded: true, elementDefs: defs});
+          this.setState({defsLoaded: true, elementDefs: defs, baseClasses: baseClasses});
         },
         error => this.setState({defsLoaded: true, error})
       );
@@ -201,139 +241,4 @@ export default class SuggestiveSearch extends React.Component {
       </div>
     );
   }
-}
-
-//todo: maybe use more hooks like this?
-function SuggestionWrapper(props) {
-  const { type, elem } = props.suggestion;
-  const { info, ix } = props;
-
-  const [isOpen, setIsOpen] = useState(false);
-  const [isDragged, setIsDragged] = useState(false);
-
-  const toggleIsOpen = () => setIsOpen(!isOpen);
-  const toggleIsDragged = () => setIsDragged(!isDragged);
-  const checkSuggestionIsOutsideSidebar = (type, elem, point, offset, ix) => {
-    if (offset.x < -300) {
-      props.onTransferSuggestionToCanvas(type, elem, point);
-      props.onDeleteSuggestionFromSidebar(ix);
-    }
-  };
-
-  //todo: investigate whether we can support suggestions of unknown things?
-  let Suggestion = null;
-  if (type.indexOf('edge') !== -1) {
-    Suggestion = <SuggestionForSelectedNode type={type} property={elem} info={info}
-                                            isOpen={isOpen} isDragged={isDragged} />;
-  } else if (type.indexOf('node') !== -1) {
-    Suggestion = <SuggestionForSelectedEdge type={type} node={elem} info={info}
-                                            isOpen={isOpen} isDragged={isDragged} />;
-  } else if (type === 'datatype') {
-    Suggestion = <SuggestionForSelectedDatatype isOpen={isOpen} isDragged={isDragged} />;
-  } else console.warn("SuggestionWrapper cannot create a suggestion for the given type " + type);
-
-  return (
-    <motion.li layout onClick={toggleIsOpen} >
-      <motion.div className={'suggestion'} layout
-                  drag dragPropagation dragConstraints={{top: 0, left: 0, right: 0, bottom: 0}} dragElastic={1}
-                  onDragStart={toggleIsDragged} onDragTransitionEnd={toggleIsDragged}
-                  onDrag={(e, i) =>
-                    checkSuggestionIsOutsideSidebar(type, elem, i.point, i.offset, ix) } >
-        {Suggestion}
-      </motion.div>
-    </motion.li>
-  );
-}
-
-function SuggestionForSelectedEdge(props) {
-  const { type, node, isOpen, isDragged } = props;
-
-  if (type === 'nodeLiteral') {
-    return (<SuggestionAsLiteral node={node} isOpen={isOpen} isDragged={isDragged} />);
-  } else {
-    const { info } = props;
-    return (<SuggestionAsNode node={node} info={info} isOpen={isOpen} isDragged={isDragged} />);
-  }
-}
-
-const variants = {
-  vis: {
-    opacity: 1,
-    transition: {
-      duration: 0.5
-    }
-  },
-  invis: {
-    opacity: 0,
-    transition: {
-      duration: 0.2
-    }
-  }
-};
-
-function SuggestionAsNode(props) {
-  const { info, node, isOpen, isDragged } = props;
-  const { expansion, label } = node;
-
-  return (
-    <>
-      <ItemImageHeader type={'nodeUri'} name={label} isDragged={isDragged} />
-      <AnimatePresence>
-        {isOpen &&
-          <motion.div className={"suggestion-extra extra"}
-                      variants={variants} initial={'invis'} animate={'vis'} exit={'invis'}>
-            <ItemPrefix prefix={expansion}/>
-            {info && <ItemDesc desc={info.comment} />}
-          </motion.div>
-        }
-      </AnimatePresence>
-    </>
-  );
-}
-
-function SuggestionAsLiteral(props) {
-  const { expansion, label } = props.node;
-  const { isOpen, isDragged } = props;
-
-  return (
-    <>
-      <ItemImageHeader type={'nodeLiteral'} name={label} isDragged={isDragged} />
-      <AnimatePresence>
-        {isOpen &&
-          <motion.div className={'suggestion-extra extra'}
-                      variants={variants} initial={'invis'} animate={'vis'} exit={'invis'} >
-            <ItemPrefix prefix={expansion} />
-          </motion.div>
-        }
-      </AnimatePresence>
-    </>
-  );
-}
-
-function SuggestionForSelectedDatatype(props) {
-  return (
-    <>
-      <motion.p>Placeholder Datatype suggestion {props}</motion.p>
-    </>
-  );
-}
-
-function SuggestionForSelectedNode(props) {
-  const { type, info, isOpen, isDragged } = props;
-  const { expansion, label } = props.property;
-
-  return (
-    <>
-      <ItemImageHeader type={type} name={label} isDragged={isDragged} />
-      <AnimatePresence>
-        {isOpen &&
-          <motion.div className={'suggestion-extra extra'}
-                      variants={variants} initial={'invis'} animate={'vis'} exit={'invis'} >
-            <ItemPrefix prefix={expansion} />
-            {info && <ItemDesc desc={info.comment} />}
-          </motion.div>
-        }
-      </AnimatePresence>
-    </>
-  );
 }
