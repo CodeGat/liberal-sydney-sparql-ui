4e13d61a43419e2e297e9e01e9a7e90ad3e094ed
Tommy Phoenix Gatti <tommy.gatti@outlook.com>
2021-05-10 15:55:01 +1000

    Added other items to the navbar, updated name

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -4,7 +4,17 @@ export default function MenuBar(props) {
 
   return (
     <div className='navbar navbar-default navbar-fixed-top'>
-
+      <div className='nav-header'>
+        <p className='nav-header-text'>LMB SPARQL Explorer</p>
+      </div>
+      <ul className='nav'>
+        <li className='nav-item'>
+          <a className='nav-item-text' href='https://lmb.cdhr.anu.edu.au/'>Return to LMB Main</a>
+        </li>
+        <li className='nav-item'>
+          <p className='nav-item-text'>Load Examples</p>
+        </li>
+      </ul>
     </div>
   );
 }
